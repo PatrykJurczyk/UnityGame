f5fab1a20a4d5c991e63a70bcdc5e07455194b83
PatrykJurczyk <jurczykpatryk8@gmail.com>
2022-12-11 15:44:24 +0100

    block double jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anim;
+
+    [SerializeField] private LayerMask jumpableGround;
+
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
-
     private enum MovementState { idle, running, jumping, falling }
-
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
@@ -24,17 +27,16 @@ public class PlayerMovement : MonoBehaviour
     {
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-        if (Input.GetButtonDown("Jump"))
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
         UpdateAnimationState();
     }
-
     private void UpdateAnimationState()
     {
         MovementState state;
-
         if (dirX > 0f)
         {
             state = MovementState.running;
@@ -49,7 +51,6 @@ public class PlayerMovement : MonoBehaviour
         {
             state = MovementState.idle;
         }
-
         if (rb.velocity.y > .1f)
         {
             state = MovementState.jumping;
@@ -60,5 +61,10 @@ public class PlayerMovement : MonoBehaviour
         }
 
         anim.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
